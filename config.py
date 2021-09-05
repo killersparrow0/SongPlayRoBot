cc6326094f9958ab7fc222f8c440400463e39962
killersparrow0 <84676044+killersparrow0@users.noreply.github.com>
2021-09-05 17:17:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
     API_ID = "4080786"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
     API_HASH = "b214aec132378022e02df812714ba7a4" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "1829524752:AAEaDBZywjMvi_NM176GNF0LurZHGBQVLME" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    BOT_TOKEN = "1849378943:AAEZTidwqccqSXt1dl_54qaVZFbwqncBrvM" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
